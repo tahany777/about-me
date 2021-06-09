1b466191f7e7b2deb5d07490943769b51df3be6f
tahany777 <tahanymohammed777@hotmail.com>
2021-06-09 10:14:10 +0300

    lab03

--- js/app.js
+++ js/app.js
@@ -4,68 +4,84 @@
 let userO = prompt('What\'s Your Name?');
 let counterGlobal = 0;
 
-alert(`Hi , ${ userO } welcome to my website`);
+alert(`Hi , ${userO} welcome to my website`);
 
 
-let qOne = prompt('Am I a good cook?').toLocaleLowerCase();
-
+let qOne = prompt('Am I a good cook?').toLowerCase();
+while (qOne !== '' || isNaN(qOne)) {
+  if (qOne === 'yes' || qOne === 'y' || qOne === 'no' || qOne === 'n') {
+    break;
+  }
+  qOne = prompt('please enter yes or no').toLowerCase();
+}
 if (qOne === 'yes' || qOne === 'y') {
   //console.log('Good Answer I love Cooking So much');
   alert('Good Answer I love Cooking So much');
   counterGlobal = counterGlobal + 1;
 } else if (qOne === 'no' || qOne === 'n') {
   alert('Incorrect Answer');
-} else {
-  alert('please enter yes or no');
 }
 
-let qTwo = prompt('my favorite Social Media is Facebook ? ').toLocaleLowerCase();
-
+let qTwo = prompt('my favorite Social Media is Facebook ? ').toLowerCase();
+while (qTwo !== '' || isNaN(qTwo)) {
+  if (qTwo === 'yes' || qTwo === 'y' || qTwo === 'no' || qTwo === 'n') {
+    break;
+  }
+  qTwo = prompt('please enter yes or no').toLowerCase();
+}
 if (qTwo === 'yes' || qTwo === 'y') {
   alert('Incorrect Answer');
 } else if (qTwo === 'no' || qTwo === 'n') {
   //console.log('Correct Answer My favorite social media is SnapChat');
   counterGlobal = counterGlobal + 1;
   alert('Correct Answer My favorite social media is SnapChat');
-} else {
-  alert('please enter yes or no');
 }
 
-let qThree = prompt('Do I Have Pets ? ').toLocaleLowerCase();
-
+let qThree = prompt('Do I Have Pets ? ').toLowerCase();
+while (qThree !== '' || isNaN(qThree)) {
+  if (qThree === 'yes' || qThree === 'y' || qThree === 'no' || qThree === 'n') {
+    break;
+  }
+  qThree = prompt('please enter yes or no').toLowerCase();
+}
 if (qThree === 'yes' || qThree === 'y') {
   alert('Incorrect Answer');
 } else if (qThree === 'no' || qThree === 'n') {
   //console.log('Correct Answer I love Pets But I don\'t have');
   counterGlobal = counterGlobal + 1;
   alert('Correct Answer I love Pets But I don\'t have ');
-} else {
-  alert('please enter yes or no');
 }
 
-let qFour = prompt('Is My Best Book "The 5-am Club" ? ').toLocaleLowerCase();
-
+let qFour = prompt('Is My Best Book "The 5-am Club" ? ').toLowerCase();
+while (qFour !== '' || isNaN(qFour)) {
+  if (qFour === 'yes' || qFour === 'y' || qFour === 'no' || qFour === 'n') {
+    break;
+  }
+  qFour = prompt('please enter yes or no').toLowerCase();
+}
 if (qFour === 'yes' || qFour === 'y') {
   //console.log('Correct Answer This Book Is Amazing');
   counterGlobal = counterGlobal + 1;
   alert('Correct Answer This Book Is Amazing ');
 } else if (qFour === 'no' || qFour === 'n') {
   alert('Incorrect Answer');
-} else {
-  alert('please enter yes or no');
 }
 
-let qFive = prompt('My favorite subject in High School is Maths ').toLocaleLowerCase();
-
+let qFive = prompt('My favorite subject in High School is Maths ').toLowerCase();
+while (qFive !== '' || isNaN(qFive)) {
+  if (qFive === 'yes' || qFive === 'y' || qFive === 'no' || qFive === 'n') {
+    break;
+  }
+  qFive = prompt('please enter yes or no').toLowerCase();
+}
 if (qFive === 'yes' || qFive === 'y') {
   alert('Incorrect Answer');
 } else if (qFive === 'no' || qFive === 'n') {
   //console.log('Correct Answer My favorite subject was Biology not Maths');
   counterGlobal = counterGlobal + 1;
   alert('Correct Answer My favorite subject was Biology not Maths');
-} else {
-  alert('please enter yes or no');
 }
+
 
 // lab 03
 
@@ -107,12 +123,12 @@ let answer = ['china', 'united states', 'japan', 'united arab emirates', 'south 
 let guess = 0;
 let count = 0;
 while (guess < 6) {
-  let userAnswer = prompt('guess One of countries with most Skyscrapers?').toLocaleLowerCase();
-  while(userAnswer === '' || !isNaN(userAnswer)) {
-    userAnswer = prompt('Please Enter An Country Name?').toLocaleLowerCase();
+  let userAnswer = prompt('guess One of countries with most Skyscrapers?').toLowerCase();
+  while (userAnswer === '' || !isNaN(userAnswer)) {
+    userAnswer = prompt('Please Enter An Country Name?').toLowerCase();
   }
   guess = guess + 1;
-  for(let i = 0; i < answer.length; i++) {
+  for (let i = 0; i < answer.length; i++) {
     if (answer[i] === userAnswer) {
       alert(`This Answer Is Correct ${answer[i]}`);
       counterGlobal = counterGlobal + 1;
@@ -121,12 +137,11 @@ while (guess < 6) {
       break;
     }
   }
-  if(count === 1) {
+  if (count === 1) {
     break;
-  } else if (guess === 6){
+  } else if (guess === 6) {
     alert(`Sorry, you can't guess the country this list of countires ${answer}`);
   }
 }
 alert(`Your Score is ${counterGlobal} Out of 7`);
-alert(`Thank You ${ userO } , For visit my website`);
-
+alert(`Thank You ${userO} , For visit my website`);
